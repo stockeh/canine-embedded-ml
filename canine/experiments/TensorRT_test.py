b479630b1bebb48e7033dc6b8af7bcc59f0970cf
Tom Cavey <tomcavey@Toms-MacBook-Pro.local>
2020-12-08 17:58:26 -0700

    Add updated tensorRT and tflite test scripts

--- canine/experiments/TensorRT_test.py
+++ canine/experiments/TensorRT_test.py
@@ -8,7 +8,7 @@ from tensorflow.python.saved_model import tag_constants
 
 INPUT_MODEL_DIR = sys.argv[1]
 
-def benchmark_saved_model(SAVED_MODEL_DIR, BATCH_SIZE=64, NUM_BATCHES=100):
+def benchmark_saved_model(SAVED_MODEL_DIR, BATCH_SIZE=1, NUM_BATCHES=64):
     saved_model_loaded = tf.saved_model.load(SAVED_MODEL_DIR, tags=[tag_constants.SERVING])
     signature_keys = list(saved_model_loaded.signatures.keys())
     print(signature_keys)
@@ -34,27 +34,17 @@ def benchmark_saved_model(SAVED_MODEL_DIR, BATCH_SIZE=64, NUM_BATCHES=100):
         
     end_time = time.time()
     print('Inference speed: %.2f samples/s'%(NUM_BATCHES*BATCH_SIZE/(end_time-start_time)))
-    
-    
-def my_input_fn():
-    for _ in range(64):
-        inp1 = np.random.rand(1, 224, 224, 3).astype(np.float32)
-        #x = tf.convert_to_tensor(np.random.rand(224, 224, 3), dtype_hint=tf.float32)
-        yield [inp1] #inp2
 
 if(len(sys.argv)>2):
+    INPUT_MODEL_DIR = sys.argv[1]
     PRECISION = sys.argv[2]
     params = tf.experimental.tensorrt.ConversionParams(
         precision_mode=PRECISION)
 
     converter = tf.experimental.tensorrt.Converter(
         input_saved_model_dir=INPUT_MODEL_DIR, conversion_params=params)
-        
     converter.convert()
-    
-    converter.build(input_fn=my_input_fn)
-    
     converter.save(INPUT_MODEL_DIR+'-trt-'+PRECISION)
     INPUT_MODEL_DIR = INPUT_MODEL_DIR+'-trt-'+PRECISION
-
-benchmark_saved_model(INPUT_MODEL_DIR, BATCH_SIZE=1, NUM_BATCHES=64)
+else:
+    benchmark_saved_model(INPUT_MODEL_DIR, BATCH_SIZE=1, NUM_BATCHES=64)
